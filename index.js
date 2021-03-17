f935a34512be4a59bf22349d7f79922020e9a7b6
Xiaochen Xu <xiaochex@gmail.com>
2021-03-17 19:50:48 -0400

    commit tree 1196502993422a25612ae18c2d8de1aa11d905a3

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = drivers => drivers.slice(0,2);
+
+const returnLastTwoDrivers = drivers => drivers.slice(2,4);
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(fair){
+    return function quad(fair){
+        return fair * 5;
+    }
+}
+
+const fareDoubler = createFareMultiplier => createFareMultiplier * 2;
+
+const fareTripler = createFareMultiplier => createFareMultiplier * 3;
+
+function selectDifferentDrivers(arrayOfDrivers,functions){
+   return functions(arrayOfDrivers);
+    }
+    
